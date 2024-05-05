d73e4a17217382338cbd5ef58a984bfab1e27328
Furkan Akgün <agnfurkan@gmail.com>
2024-05-05 20:19:47 +0300

    Add project files.

---
+++ Fbu.AdvertisementApp.Entities/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fbu.AdvertisementApp.Entities
+{
+   public class AppUser :BaseEntity
+    {
+        public string Username {  get; set; }
+
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public int GenderId {  get; set; }
+        public Gender Gender { get; set; }
+
+        public List<AppUserRole> AppUserRoles {  get; set; }
+    }
+
+}
---
+++ Fbu.AdvertisementApp.Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Fbu.AdvertisementApp.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+
+    }
+}
